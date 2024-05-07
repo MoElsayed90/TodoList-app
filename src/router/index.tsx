61fbd12b09a0a77ac9736bb0f4c3438bbcee9e55
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-05-07 12:46:26 +0300

    Update "add route Register"

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,16 +5,17 @@ import {
   Route,
 } from "react-router-dom";
 import RegisterPage from "../pages/Register";
+import RootLayout from "../pages/Layout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route
-        path="/"
-        element={<h3>home</h3>} />
+        <Route path="/" element={<RootLayout/>}>
+
       <Route
         path="register"
         element={<RegisterPage />} />
+        </Route>
     </>
   )
 );
