b96844a176c45416a1c35a1feb492e02de6d3f99
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-05-07 12:47:38 +0300

    Update "add Register Page"

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -6,8 +6,8 @@ const RegisterPage = () => {
 
   return (
     <>
-      <div className=" flex flex-col space-y-4 max-w-md mx-auto content-center mt-48  items-center">
-        <h2 className="font-semibold text-3xl  capitalize">
+      <div className=" flex flex-col space-y-4  mt-48 max-w-md mx-auto">
+        <h2 className="font-semibold text-3xl  capitalize text-center">
           register to get access !
         </h2>
         <form className="space-y-4 flex flex-col w-full">
@@ -17,18 +17,7 @@ const RegisterPage = () => {
           <Button className="bg-indigo-500 hover:bg-indigo-700 capitalize">register</Button>
         </form>
       </div>
-      {/* <div className="max-w-md mx-auto flex w-full">  
-     <h2 className="text-center mb-4 text-3xl font-semibold">
-    //     Register to get access!
-    //   </h2>
-    //   <form className="space-y-4 ">
-    //     <Input placeholder="Username"/>
-    //     <Input placeholder="Email address"/>
-    //     <Input placeholder="Password"/>
-    //     <Button className="mt-5 bg-neutral-700 hover:bg-neutral-900">Register</Button>
-    //   </form>
-    // </div> */}
-    </>
+      </>
   );
 };
 
