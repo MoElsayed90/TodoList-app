9386227242a40c2bfebb9700a1d2d77e19f623fd
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-05-07 19:14:14 +0300

    update"add TODOLIst Page and Home Page"

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import TodoList from "../components/TodoList";
+
+const HomePage = () => {
+  return (
+    <div>
+      <section className="max-w-2xl mx-auto">
+        <TodoList />
+      </section>
+    </div>
+  )
+
+}
+
+export default HomePage;
