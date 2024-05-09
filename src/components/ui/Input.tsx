8c5a1d2b8c2b9449feb8c430603d3d7eebea6a37
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-05-09 17:58:46 +0300

    fix "fix error input"

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable react-refresh/only-export-components */
-import { FC, InputHTMLAttributes, memo } from "react"
+import { FC, InputHTMLAttributes, Ref, forwardRef, memo } from "react"
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {
 
 }
-const Input: FC<IProps> = ({ ...rest }) => {
-    return <input className=" border-[1px] border-gray-300 shadow-lg focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:duration-300 rounded-md px-3 py-3 text-md " {...rest}/>
-}
+const Input: FC<IProps> = forwardRef(({ ...rest },ref:Ref<HTMLInputElement>) => {
+    return <input   
+    ref={ref}
+    className=" border-[1px] border-gray-300 shadow-lg focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:duration-300 rounded-md px-3 py-3 text-md " {...rest} 
+    />
+})
 export default memo(Input);
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,20 +1,30 @@
 import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
+import { useForm, SubmitHandler } from "react-hook-form"
+interface IFormInput {
+  username:string;
+  email:string;
+  password:string;
+}
 
 
 const RegisterPage = () => {
 
+    const { register, handleSubmit , formState:{errors} } = useForm<IFormInput>()
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+  
+    console.log(errors)
   return (
     <>
       <div className=" flex flex-col space-y-4  mt-48 max-w-md mx-auto">
         <h2 className="font-semibold text-3xl  capitalize text-center">
           register to get access !
         </h2>
-        <form className="space-y-4 flex flex-col w-full">
-          <Input placeholder="Username"/>
-          <Input placeholder="Email" />
-          <Input placeholder="Password" />
-          <Button className="bg-indigo-500 hover:bg-indigo-700 capitalize">register</Button>
+        <form className="space-y-4 flex flex-col w-full" onSubmit={handleSubmit(onSubmit)}>
+          <Input placeholder="Username"{...register("username",{required:"Username is Required"})} />
+          <Input placeholder="Email"{...register("email",{required:"Email is Required"})} />
+          <Input placeholder="Password"{...register("password",{required:"password is Required"})}  />
+          <Button className="bg-indigo-500 hover:bg-indigo-700 capitalize" type="submit">register</Button>
         </form>
       </div>
       </>
