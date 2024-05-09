5c9a2eb48e26fe0e06114b3524680f4b266eca05
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-05-10 02:16:10 +0300

    update "add Register Api"

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -6,6 +6,8 @@ import Input from "../components/ui/Input";
 import { useForm, SubmitHandler } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import { RegisterSchema } from "../components/validation";
+import axiosInstance from "../config/axios.config";
+import toast, { Toaster } from "react-hot-toast";
 interface IFormInput {
   username: string;
   email: string;
@@ -15,8 +17,26 @@ interface IFormInput {
 
 const RegisterPage = () => {
 
-  const { register, handleSubmit, formState: { errors } } = useForm<IFormInput>( {resolver: yupResolver(RegisterSchema)})
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+  const { register, handleSubmit, formState: { errors } } = useForm<IFormInput>({ resolver: yupResolver(RegisterSchema) })
+  const onSubmit: SubmitHandler<IFormInput> = async data => {
+    console.log(data)
+    try {
+      const { status } = await axiosInstance.post("/auth/local/register", data);
+        if(status == 200){
+          toast.success('Successfully created!',{
+            position:"bottom-center",
+            duration:4000,
+            style:{
+              backgroundColor:"black",
+              color:"white",
+              width: "fit-content"
+            }
+          })
+        }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   console.log(errors)
 
@@ -34,12 +54,13 @@ const RegisterPage = () => {
           register to get access !
         </h2>
         <form className="space-y-4 flex flex-col w-full" onSubmit={handleSubmit(onSubmit)}>
-            {renderRegisterForm}
+          {renderRegisterForm}
 
 
           <Button className="bg-indigo-500 hover:bg-indigo-700 capitalize" type="submit">register</Button>
         </form>
       </div>
+      <Toaster />
     </>
   );
 };
