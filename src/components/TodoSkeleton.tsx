ba0c695e9cbcd1daaa709698df12f1e7972e9487
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-05-16 00:51:07 +0300

    update "add skelaton'

--- src/components/TodoSkeleton.tsx
+++ src/components/TodoSkeleton.tsx
@@ -1,12 +1,10 @@
 const TodoSkeleton = () => {
   return (
-    <div className="flex items-center justify-between">
-      <div>
-        <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-      </div>
+    <div className="flex items-center justify-between space-y-1  space-x-2 ">
+      <div className="w-full py-3 bg-gray-200 rounded-full dark:bg-gray-700"></div>
       <div className="flex items-center space-x-2">
-        <div className="h-9 bg-gray-300 rounded-md dark:bg-gray-700 w-12"></div>
-        <div className=" h-9 bg-gray-300 rounded-md dark:bg-gray-700 w-12"></div>
+        <div className="h-9 bg-gray-300 rounded-md dark:bg-gray-700 w-20 "></div>
+        <div className=" h-9 bg-gray-300 rounded-md dark:bg-gray-700 w-20 "></div>
       </div>
     </div>
   );
