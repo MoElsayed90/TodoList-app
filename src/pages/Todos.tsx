be6b2bc687909aea36a79b22ff8c53f1270b1f63
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-05-22 06:52:22 +0300

    update "add todos page"

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,45 +1,78 @@
+import useAuthenticationQuery from "../hooks/useAuthenticationQuery";
+import TodoSkeleton from "../components/TodoSkeleton";
 import Button from "../components/ui/Button";
+import axiosInstance from "../config/axios.config";
+import { faker } from "@faker-js/faker";
+import Paginator from "../components/ui/Paginator";
+import { useState } from "react";
+
+
 
 const Todos = () => {
-  return (
-    <>
-      <div className="flex items-center justify-center space-x-2">
-        <Button className="bg-indigo-500 hover:bg-indigo-700 capitalize w-1/6">
-          Generate todos
-        </Button>
-        <div className="flex items-center justify-between space-x-2 text-md">
-          <select className="border-2 border-indigo-600 rounded-md p-2"
-          >
-            <option disabled>Sort by</option>
-            <option value="ASC">Oldest</option>
-            <option value="DESC">Latest</option>
-          </select>
-          <select className="border-2 border-indigo-600 rounded-md p-2" >
-            <option disabled>Page Size</option>
-            <option value={10}>10</option>
-            <option value={50}>50</option>
-            <option value={100}>100</option>
-          </select>
-        </div>
-      </div>
-      <div className="my-20 space-y-6">
-        <div className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-          <h3 className="w-full font-semibold">1-title</h3>
-        </div>
-      </div>
-      <div className="my-20 space-y-6">
-        <div className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-          <h3 className="w-full font-semibold">1-title</h3>
-        </div>
-      </div>
-      <div className="my-20 space-y-6">
-        <div className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-          <h3 className="w-full font-semibold">1-title</h3>
-        </div>
-      </div>
+  const [page, setPage] = useState<number>(1);
+  const storageKey = "loggedInUser";
+  const userDataString = localStorage.getItem(storageKey)
+  const userData = userDataString ? JSON.parse(userDataString) : null;
+  const { isPending, data } = useAuthenticationQuery({
+    queryKey: ["Todo",`${page}`],
+    url: "todos",
+    config: {
+      headers: {
+        Authorization: `Bearer ${userData.jwt}`,
+      }
+    }
+  })
+  const GenerateFakeData = async () => {
+    for (let i = 0; i < 100; i++) {
 
-    </>
-  );
-};
+      try {
+        await axiosInstance.post(`/todos`, { data: { title :faker.word.words(5), description:faker.lorem.paragraph(3), user: [userData.user.id] } }, {
+          headers: {
+            Authorization: `Bearer ${userData.jwt}`
+          }
+        })
+
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+  }
+  console.log(data)
+  if (isPending) return <div className="mt-24">{Array.from({ length: 3 }, (_, idx) => <TodoSkeleton key={idx} />)}</div>
+      /** Handlers */
+      const handlerOnClickPrev = () => {
+        setPage(prev => prev -1)
+      }  
+      const handlerOnClickNext = () => {
+        setPage(prev => prev +1)
+      }  
+  
+  return (
+
+    <div className="flex flex-col justify-center items-center  mx-auto my-10 space-y-4 ">
+      <div className=" flex justify-center items-center">
+      <Button variant="outline" size={"sm"} onClick={GenerateFakeData} className="w-52 ">
+        Generate todos
+      </Button>
+      </div>
+      {
+        data.data.map(({ attributes, id }: { id: number; attributes: { title: string } }) => (
+          <>
+            <div key={id} className=" space-x-4 flex  w-1/2 hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
+              
+              <p className=" font-semibold">
+                {id} - {attributes.title}
+              </p>
+
+            </div>
+          </>
+        ))
+      }
+      <Paginator page={page} pageCount={3} setPrev={handlerOnClickPrev} setNext={handlerOnClickNext}/>
+      </div>
+  )
+
+}
 
 export default Todos;
